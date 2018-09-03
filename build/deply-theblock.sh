864180738d92be35bd4bfb4e24e4d4f9151a709c
Duri Bradshaw <duri.bradshaw@gmail.com>
2018-09-03 19:54:26 +1000

    deply script

--- build/deply-theblock.sh
+++ build/deply-theblock.sh
@@ -1,1 +1,1 @@
-aws s3 cp ~/code/duri-rocks/maps/the-block/ s3://duri.rocks/maps/the-block/ --recursive --acl public-read --exclude ".git/*" --exclude ".*" --exclude "*.bat" --exclude "*.md" --exclude "LICENSE"
+aws s3 cp ~/code/duri-rocks/maps/the-block/ s3://duri.rocks/maps/the-block/ --recursive --acl public-read --exclude ".*" --exclude "*.bat" --exclude "*.sh" --exclude "*.md" --exclude "LICENSE"
